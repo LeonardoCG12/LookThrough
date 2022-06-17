b187784b85fbabe9138a76daa3d79926cb50ed29
Leonardo Gouvea <lcgstack12@gmail.com>
2022-06-17 05:44:39 -0300

    Adicionado struct

--- main.go
+++ main.go
@@ -10,49 +10,49 @@ import (
 	"strings"
 )
 
-var (
+type vars struct {
 	fileCount   int
 	hashCount   int
 	hashList    []string
 	hashListAll []string
-	home, _     = os.UserHomeDir()
+	home        string
 	num         string
-	mem         = map[string]int{}
+	mem         map[string]int
 	newPath     string
 	myPath      string
-	sep         = fmt.Sprintf("%c", os.PathSeparator)
-)
+	sep         string
+}
 
-func makeNewDir() {
+func (v *vars) makeNewDir() {
 
-	if myPath != "" {
+	if v.myPath != "" {
 		return
 	}
 
-	refactorPath()
+	v.refactorPath()
 
-	myPath = strings.TrimSuffix(myPath, sep)
-	arr := strings.Split(myPath, sep)
-	newPath = fmt.Sprintf("%s/new-%s", myPath, arr[len(arr)-1])
+	v.myPath = strings.TrimSuffix(v.myPath, v.sep)
+	arr := strings.Split(v.myPath, v.sep)
+	v.newPath = fmt.Sprintf("%s/new-%s", v.myPath, arr[len(arr)-1])
 
-	os.Mkdir(newPath, 0755)
+	os.Mkdir(v.newPath, 0755)
 }
 
-func refactorPath() string {
+func (v *vars) refactorPath() string {
 
 	if len(os.Args) > 1 {
-		myPath = os.Args[1]
+		v.myPath = os.Args[1]
 	} else {
 		fmt.Print("Choose a directory to look through: ")
-		fmt.Scanf("%s", &myPath)
+		fmt.Scanf("%s", &v.myPath)
 	}
 
-	myPath = strings.Replace(myPath, "~", home, -1)
+	v.myPath = strings.Replace(v.myPath, "~", v.home, -1)
 
-	return myPath
+	return v.myPath
 }
 
-func getMD5Checksum(filePath string, fileName string) {
+func (v *vars) getMD5Checksum(filePath string, fileName string) {
 	file, err := ioutil.ReadFile(filePath)
 
 	if err != nil {
@@ -68,26 +68,26 @@ func getMD5Checksum(filePath string, fileName string) {
 
 	checksum := fmt.Sprintf("%x", hasher.Sum(nil))
 
-	saveHash(fileName, file, checksum)
+	v.saveHash(fileName, file, checksum)
 }
 
-func saveHash(fileName string, file []byte, md5Sum string) {
-	value, isThere := mem[fileName]
-	look4Hash := look4Hashes(fileName, md5Sum)
+func (v *vars) saveHash(fileName string, file []byte, md5Sum string) {
+	value, isThere := v.mem[fileName]
+	look4Hash := v.look4Hashes(fileName, md5Sum)
 
 	if look4Hash == 1 {
-		hashCount += 1
+		v.hashCount += 1
 
 		if isThere {
-			mem[fileName] += 1
-			num = fmt.Sprintf("%d", value+1)
+			v.mem[fileName] += 1
+			v.num = fmt.Sprintf("%d", value+1)
 		} else {
-			num = ""
+			v.num = ""
 		}
 
-		hashList = append(hashList, fileName, md5Sum)
+		v.hashList = append(v.hashList, fileName, md5Sum)
 		arr := strings.Split(fileName, ".")
-		filePath := fmt.Sprintf("%s%s%s(%s).%s", newPath, sep, arr[0], num, arr[len(arr)-1])
+		filePath := fmt.Sprintf("%s%s%s(%s).%s", v.newPath, v.sep, arr[0], v.num, arr[len(arr)-1])
 		err := ioutil.WriteFile(filePath, file, 0644)
 
 		if err != nil {
@@ -95,11 +95,11 @@ func saveHash(fileName string, file []byte, md5Sum string) {
 		}
 
 	} else if look4Hash == 2 {
-		hashCount += 1
-		mem[fileName] = 0
+		v.hashCount += 1
+		v.mem[fileName] = 0
 
-		hashList = append(hashList, fileName, md5Sum)
-		filePath := fmt.Sprintf("%s%s%s", newPath, sep, fileName)
+		v.hashList = append(v.hashList, fileName, md5Sum)
+		filePath := fmt.Sprintf("%s%s%s", v.newPath, v.sep, fileName)
 		err := ioutil.WriteFile(filePath, file, 0644)
 
 		if err != nil {
@@ -108,13 +108,13 @@ func saveHash(fileName string, file []byte, md5Sum string) {
 
 	}
 
-	hashListAll = append(hashListAll, fileName, md5Sum)
+	v.hashListAll = append(v.hashListAll, fileName, md5Sum)
 
 }
 
-func look4Hashes(fileName string, md5Sum string) int {
+func (v *vars) look4Hashes(fileName string, md5Sum string) int {
 
-	for _, element := range hashList {
+	for _, element := range v.hashList {
 
 		if md5Sum == element {
 			return 0
@@ -122,7 +122,7 @@ func look4Hashes(fileName string, md5Sum string) int {
 
 	}
 
-	for _, element := range hashList {
+	for _, element := range v.hashList {
 
 		if fileName == element {
 			return 1
@@ -133,12 +133,12 @@ func look4Hashes(fileName string, md5Sum string) int {
 	return 2
 }
 
-func look4Files() {
-	arr := strings.Split(newPath, sep)
+func (v *vars) look4Files() {
+	arr := strings.Split(v.newPath, v.sep)
 	newPathDir := arr[len(arr)-1]
 
-	err := filepath.Walk(myPath, func(path string, info os.FileInfo, err error) error {
-		arr = strings.Split(path, sep)
+	err := filepath.Walk(v.myPath, func(path string, info os.FileInfo, err error) error {
+		arr = strings.Split(path, v.sep)
 		checkDir := arr[len(arr)-2]
 
 		if err != nil {
@@ -146,8 +146,8 @@ func look4Files() {
 		}
 
 		if !info.IsDir() && checkDir != newPathDir {
-			fileCount += 1
-			getMD5Checksum(path, info.Name())
+			v.fileCount += 1
+			v.getMD5Checksum(path, info.Name())
 		}
 
 		return nil
@@ -157,11 +157,11 @@ func look4Files() {
 		log.Fatal(err)
 	}
 
-	if verifyFiles() {
+	if v.verifyFiles() {
 		fmt.Print("\n[+] SUCCESS\n")
 		fmt.Print("[+] ALL FILES HAVE BEEN COPIED\n")
-		fmt.Printf("\n>>> Old Files: %d\n", fileCount)
-		fmt.Printf(">>> New Files: %d\n\n", hashCount)
+		fmt.Printf("\n>>> Old Files: %d\n", v.fileCount)
+		fmt.Printf(">>> New Files: %d\n\n", v.hashCount)
 	} else {
 		fmt.Print("\n[-] FAIL\n")
 		fmt.Print("[-] SOMETHING WENT WRONG\n\n")
@@ -169,16 +169,16 @@ func look4Files() {
 
 }
 
-func verifyFiles() bool {
+func (v *vars) verifyFiles() bool {
 	var integrity bool
 
 start:
 
-	for i := 1; i < len(hashListAll); i += 2 {
+	for i := 1; i < len(v.hashListAll); i += 2 {
 
-		for j := 1; j < len(hashList); j += 2 {
+		for j := 1; j < len(v.hashList); j += 2 {
 
-			if hashListAll[i] == hashList[j] {
+			if v.hashListAll[i] == v.hashList[j] {
 				integrity = true
 				continue start
 			} else {
@@ -193,6 +193,21 @@ start:
 }
 
 func main() {
-	makeNewDir()
-	look4Files()
+	home, _ := os.UserHomeDir()
+
+	iv := vars{
+		fileCount:   0,
+		hashCount:   0,
+		hashList:    []string{},
+		hashListAll: []string{},
+		home:        home,
+		num:         "",
+		mem:         map[string]int{},
+		newPath:     "",
+		myPath:      "",
+		sep:         fmt.Sprintf("%c", os.PathSeparator),
+	}
+
+	iv.makeNewDir()
+	iv.look4Files()
 }
